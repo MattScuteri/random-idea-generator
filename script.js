9b6f2dd71a4165dc7771c187eab895c6e624c158
MattScuteri <mttscuteri@gmail.com>
2019-02-14 01:57:39 -0500

    hide and show results div on click

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let adjective = document.getElementById('adjective');
-
 function getWords() {
     const api = "http://api.datamuse.com/words?rel_jjb=";
     const noun = document.getElementById("noun").value;
-
+    let adjective = document.getElementById('adjective');
+    let userNoun = document.getElementById('user-noun');
     
     var url = api + noun
 
-    console.log(url);
     request = new XMLHttpRequest();
     request.open("GET", url);
     request.onload = function() {
@@ -16,8 +14,10 @@ function getWords() {
 
         let randomAdjective = data[Math.floor(Math.random()*data.length)];
         console.log(randomAdjective.word);
-        document.getElementById("adjective").innerHTML = randomAdjective.word;
-        document.getElementById("user-noun").innerHTML = noun;
+
+        document.getElementById('results-div').style.display = "block";
+        adjective.innerHTML = randomAdjective.word;
+        userNoun.innerHTML = noun;
     }
     request.send();
 }
